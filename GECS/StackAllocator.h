4c41e1c1ad9341bf83826d65f5cf9d949777e1ac
Alexey Golubev <agolubev617@gmail.com>
2025-01-09 21:23:53 +0300

    Added stack allocator header

---
+++ GECS/StackAllocator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "API.h"
+#include "Memory/IAllocator.h"
+
+namespace GECS {
+	namespace Memory {
+		struct StackAllocatorHeader {
+			u8 offset;
+		};
+
+		class StackAllocator : public IAllocator {
+		public:
+			StackAllocator(size_t memorySize, const uptr firstSize);
+			virtual ~StackAllocator();
+
+			virtual uptr Allocate(size_t size, u8 align) override;
+			virtual void Free(uptr address) override;
+			virtual void Clear() override;
+		};
+	}
+}
